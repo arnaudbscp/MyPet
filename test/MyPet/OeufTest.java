daac71bae71e8066588e33b17ee3b39c99023428
Younoussa SOW <younoussa.sow.etu@univ-lille.fr>
2018-09-05 17:38:24 +0200

    README.md sprint-2

---
+++ test/MyPet/OeufTest.java
@@ -0,0 +1,30 @@
+package MyPet;
+
+import static org.junit.Assert.*;
+
+public class OeufTest {
+
+    @org.junit.Test
+    public void soigner() {
+    }
+
+    @org.junit.Test
+    public void dormir() {
+    }
+
+    @org.junit.Test
+    public void laver() {
+    }
+
+    @org.junit.Test
+    public void caresser() {
+    }
+
+    @org.junit.Test
+    public void mangerBoire() {
+    }
+
+    @org.junit.Test
+    public void jouer() {
+    }
+}
