d71f78af320f19afafcae8b8ae1b2f54b77bb740
BASCOP Arnaud <arnaud.bascop@univ-lille1.fr>
2018-09-05 14:47:13 +0200

    tempo

---
+++ src/MyPet/Oeuf.java
@@ -0,0 +1,47 @@
+package MyPet;
+
+public class Oeuf extends Personnage implements Actions {
+
+	public Oeuf(String prenom) {
+		super(prenom);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int soigner() {
+		
+	}
+
+	@Override
+	public int dormir() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int laver() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int caresser() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mangerBoire() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int jouer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
