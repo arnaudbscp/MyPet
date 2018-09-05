94d42125da7fecd5de4805a4477b67523e3b0948
BASCOP Arnaud <arnaud.bascop@univ-lille1.fr>
2018-09-05 14:35:38 +0200

    Actions et Personnage

---
+++ src/MyPet/Actions.java
@@ -0,0 +1,10 @@
+package MyPet;
+
+public interface Actions {
+	int soigner();
+	int dormir();
+	int laver();
+	int caresser();
+	int mangerBoire();
+	int jouer();
+}
--- src/MyPet/Personnage.java
+++ src/MyPet/Personnage.java
@@ -1,5 +1,50 @@
 package MyPet;
 
 public abstract class Personnage {
+	private String prenom;
+	private int energie;
+	private int faim;
+	private int bonheur;
+	private int sante;
+	
+	public Personnage(String prenom) {
+		this.prenom = prenom;
+	}
 
+	public int getEnergie() {
+		return energie;
+	}
+
+	public void setEnergie(int energie) {
+		this.energie = energie;
+	}
+
+	public int getFaim() {
+		return faim;
+	}
+
+	public void setFaim(int faim) {
+		this.faim = faim;
+	}
+
+	public int getBonheur() {
+		return bonheur;
+	}
+
+	public void setBonheur(int bonheur) {
+		this.bonheur = bonheur;
+	}
+
+	public int getSante() {
+		return sante;
+	}
+
+	public void setSante(int sante) {
+		this.sante = sante;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+	
 }
