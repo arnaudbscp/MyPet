69bf414fbe2aee79079ff1dbc659d6441f9a08a9
Younoussa SOW <younoussa.sow.etu@univ-lille.fr>
2018-09-06 08:50:19 +0200

    update Partie.java: input validation Create new MainApp for replace main.java Update Print Pet

---
+++ src/MyPet/MainApp.java
@@ -0,0 +1,41 @@
+package MyPet;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Partie partie = new Partie("Caliméro");
+
+		Personnage perso = partie.getPet();
+		
+		while(!perso.estMort()) {
+			PetAfficheur.printPersonnage(perso);
+			int choix  = partie.lireInstruction();
+			switch(choix) {
+				case 1:
+					perso.mangerBoire();
+					break;
+				case 2:
+					perso.dormir();
+					break;
+				case 3:
+					perso.soigner();
+					break;
+				case 4:
+					perso.caresser();
+					break;
+				case 5:
+					perso.jouer();
+					break;
+				case 6:
+					perso.laver();
+					break;
+				default:
+					System.out.println("Instruction incorrecte.");
+				}
+		}
+		
+		PetAfficheur.printPersonnage(perso);
+
+	}
+
+}
