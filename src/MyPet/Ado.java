68651751ce9b9099abf0e4dadaecd619fcc80630
Brice Despelchin <bricedespelchin@gmail.com>
2018-09-06 10:24:36 +0200

    \n sur ado

--- src/MyPet/Ado.java
+++ src/MyPet/Ado.java
@@ -61,8 +61,8 @@ public class Ado extends Personnage {
     
     @Override
 	public String messageInit() {
-		return "__Me voici à l'âge ingrat... Je risque d'être plus facilement de mauvaise humeur et d'avoir faim, très faim !__"
-				+ "Mais j'ai confiance en toi, je sais que tu vas t'en sortir avec moi !"
+		return "__Me voici à l'âge ingrat... Je risque d'être plus facilement de mauvaise humeur et d'avoir faim, très faim !__\n"
+				+ "Mais j'ai confiance en toi, je sais que tu vas t'en sortir avec moi !\n"
 				+ "C'EST A TOI !\n";
 	}
 
