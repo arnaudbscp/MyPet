659d87596ea2be7f908117a5a168b3706ecd0d1f
BASCOP Arnaud <arnaud.bascop@univ-lille1.fr>
2018-09-06 14:31:14 +0200

    mort

--- src/MyPet/MainApp.java
+++ src/MyPet/MainApp.java
@@ -15,79 +15,77 @@ public class MainApp {
 			perso = partie.getPet();
 			tempo = perso.getTempo();
 			
-			if(perso.estAffame()) {
-				perso.perteSante();
-				System.out.println("J'ai faim, je vais finir par tomber malade !");
-			}
-				
-			if(perso.estTriste()) {
-				perso.perteEnergie();
-				System.out.println("Je suis trop triste, ça me fatigue !");
-			}
-			
-			PetAfficheur.printPersonnage(perso);
-			
-			int choix  = partie.lireInstruction();
-			switch(choix) {
-				case 1:
-					if(perso.getCaract() == Caractere.Rassasie && hasard()) {
-						System.out.println("Il ne veut pas de nourriture.");
-					}else {
-						tempo += perso.mangerBoire();
-						if(e.seProduit()) {
-						e.estMalade();
-						}
-					}
-					break;
-				case 2:
-					if(perso.getCaract() == Caractere.Joyeux && hasard()) {
-						System.out.println("Il n'est pas d'humeur à dormir, il veut s'amuser.");
-					}else {
-						tempo += perso.dormir();
-						if(e.seProduit()) {
-							e.peurDuNoir();
-						}
-					}
-					break;
-				case 3:
-					tempo += perso.soigner();
-					if(e.seProduit()) {
-						e.seSentTriste();
-					}
-					break;
-				case 4:
-					tempo += perso.caresser();
-					break;
-				case 5:
-					if(perso.getCaract() == Caractere.Feneant && hasard()) {
-						System.out.println("Il n'est pas d'humeur à jouer.");
-					}else {
-						tempo += perso.jouer();
-					}
-					break;
-				case 6:
-					tempo += perso.laver();
-					break;
-				default:
-					System.out.println("Instruction incorrecte.");
+			if(!perso.estMort()) {
+				if(perso.estAffame()) {
+					perso.perteSante();
+					System.out.println("J'ai faim, je vais finir par tomber malade !");
 				}
-			perso.effetCaractere(perso);
-			perso.verifiePalier();
-			perso.setTempo(tempo);
-			
-			if(partie.testJourneeEcoulee());
-			
-			System.out.println("Jour numéro " + partie.getCompteurJours());
-			
-			partie.evolution();
+					
+				if(perso.estTriste()) {
+					perso.perteEnergie();
+					System.out.println("Je suis trop triste, ça me fatigue !");
+				}
+				
+				PetAfficheur.printPersonnage(perso);
+				
+				int choix  = partie.lireInstruction();
+				switch(choix) {
+					case 1:
+						if(perso.getCaract() == Caractere.Rassasie && hasard()) {
+							System.out.println("Il ne veut pas de nourriture.");
+						}else {
+							tempo += perso.mangerBoire();
+							if(e.seProduit()) {
+							e.estMalade();
+							}
+						}
+						break;
+					case 2:
+						if(perso.getCaract() == Caractere.Joyeux && hasard()) {
+							System.out.println("Il n'est pas d'humeur à dormir, il veut s'amuser.");
+						}else {
+							tempo += perso.dormir();
+							if(e.seProduit()) {
+								e.peurDuNoir();
+							}
+						}
+						break;
+					case 3:
+						tempo += perso.soigner();
+						if(e.seProduit()) {
+							e.seSentTriste();
+						}
+						break;
+					case 4:
+						tempo += perso.caresser();
+						break;
+					case 5:
+						if(perso.getCaract() == Caractere.Feneant && hasard()) {
+							System.out.println("Il n'est pas d'humeur à jouer.");
+						}else {
+							tempo += perso.jouer();
+						}
+						break;
+					case 6:
+						tempo += perso.laver();
+						break;
+					default:
+						System.out.println("Instruction incorrecte.");
+					}
+				perso.effetCaractere(perso);
+				perso.verifiePalier();
+				perso.setTempo(tempo);
+				
+				if(partie.testJourneeEcoulee());
+				
+				System.out.println("Jour numéro " + partie.getCompteurJours());
+				
+				partie.evolution();
+			}
 		}
 		
 		PetAfficheur.printPersonnage(perso);
-
-		if(perso.estMort()) {
-			System.out.println("Il est mort prématurément, le pauvre.");
-		}
-
+		System.out.println("Il est mort prématurément, le pauvre.");
 	}
 
 	public static boolean hasard() {
